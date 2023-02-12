e81444c67cc4f4f5ccaf11697a8fa713ddb1ecb6
Khaled Fathi <mail@khaledfathi.com>
2023-02-12 23:20:38 +0200

    `

--- aa.php
+++ aa.php
@@ -35,3 +35,77 @@ return new class extends Migration
         Schema::dropIfExists('transactions');
     }
 };
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_type_id')->nullable(false)->references('id')->on('customers');
+            $table->foreignId('customer_id')->nullable(true)->references('id')->on('customers');
+            $table->foreignId('project_id')->nullable(true)->references('id')->on('projects');
+            $table->date('date')->nullable(false);
+            $table->time('time')->nullable(false);
+            $table->bigInteger('amount')->nullable(false);
+            $table->text('details')->nullable(false); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_type_id')->nullable(false)->references('id')->on('customers');
+            $table->foreignId('customer_id')->nullable(true)->references('id')->on('customers');
+            $table->foreignId('project_id')->nullable(true)->references('id')->on('projects');
+            $table->date('date')->nullable(false);
+            $table->time('time')->nullable(false);
+            $table->bigInteger('amount')->nullable(false);
+            $table->text('details')->nullable(false); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
